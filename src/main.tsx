53647aecfb488a34eb66e87c989e161b530cf475
Franck Ernewein <franck.ernewein@swile.co>
2023-01-22 19:26:10 +0900

    basic ui styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 import App from "./App";
+import "./style.css";
 
 const queryClient = new QueryClient();
 
