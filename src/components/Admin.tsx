2e3140f3b53c0083093f9e2c589ae2e1d994ee06
Franck Ernewein <franck.ernewein@swile.co>
2023-02-04 11:54:37 +0900

    admin skin

--- src/components/Admin.tsx
+++ src/components/Admin.tsx
@@ -5,6 +5,9 @@ import startGame from "../commands/startGame";
 import startRound from "../commands/startRound";
 import endBet from "../commands/endBet";
 import endRound from "../commands/endRound";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
 
 const Admin: FC<Game> = (game) => {
   const startGameMutation = useCommand(startGame);
@@ -12,63 +15,73 @@ const Admin: FC<Game> = (game) => {
   const endBetMutation = useCommand(endBet);
   const endRoundMutation = useCommand(endRound);
   return (
-    <div>
-      <h2>Admin</h2>
-      <hr />
-      <button
-        onClick={() => startGameMutation.mutate({})}
-        disabled={startGameMutation.isLoading}
-      >
-        {game.id ? "reset game" : "start game"}
-      </button>
-      <hr />
-      {game.id && (
-        <div>
-          {!game.currentRound && (
-            <button
-              onClick={() => {
-                startRoundMutation.mutate({});
-              }}
-              disabled={startRoundMutation.isLoading}
-            >
-              start a new round
-            </button>
-          )}
-          {game.currentRound &&
-            game.currentRound?.status === RoundStatus.BET_TIME && (
-              <button
-                onClick={() => {
-                  endBetMutation.mutate({});
-                }}
-                disabled={endBetMutation.isLoading}
-              >
-                end bet
-              </button>
-            )}
-          {game.currentRound &&
-            game.currentRound?.status === RoundStatus.RUNNING && (
-              <div>
-                <button
-                  onClick={() => {
-                    endRoundMutation.mutate({ roundResult: RoundResult.WIN });
-                  }}
-                  disabled={endRoundMutation.isLoading}
-                >
-                  Round won
-                </button>
-                <button
-                  onClick={() => {
-                    endRoundMutation.mutate({ roundResult: RoundResult.LOSE });
-                  }}
-                  disabled={endRoundMutation.isLoading}
-                >
-                  Round Lost
-                </button>
-              </div>
-            )}
-        </div>
+    <Grid container>
+      <Grid item xs={12}>
+        <Typography>Admin</Typography>
+      </Grid>
+      <Grid item xs={4}>
+        <Button
+          variant="outlined"
+          onClick={() => startGameMutation.mutate({})}
+          disabled={startGameMutation.isLoading}
+        >
+          {game.id ? "reset game" : "start game"}
+        </Button>
+      </Grid>
+      {game.id && !game.currentRound && (
+        <Grid item xs={4}>
+          <Button
+            variant="outlined"
+            onClick={() => {
+              startRoundMutation.mutate({});
+            }}
+            disabled={startRoundMutation.isLoading}
+          >
+            start a new round
+          </Button>
+        </Grid>
       )}
-    </div>
+      {game.currentRound && game.currentRound?.status === RoundStatus.BET_TIME && (
+        <Grid item xs={4}>
+          <Button
+            variant="outlined"
+            onClick={() => {
+              endBetMutation.mutate({});
+            }}
+            disabled={endBetMutation.isLoading}
+          >
+            end bet
+          </Button>
+        </Grid>
+      )}
+      {game.currentRound && game.currentRound?.status === RoundStatus.RUNNING && (
+        <>
+          <Grid item xs={2}>
+            <Button
+              variant="contained"
+              onClick={() => {
+                endRoundMutation.mutate({ roundResult: RoundResult.WIN });
+              }}
+              disabled={endRoundMutation.isLoading}
+            >
+              Round won
+            </Button>
+          </Grid>
+          <Grid item xs={2}>
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={() => {
+                endRoundMutation.mutate({ roundResult: RoundResult.LOSE });
+              }}
+              disabled={endRoundMutation.isLoading}
+            >
+              Round Lost
+            </Button>
+          </Grid>
+        </>
+      )}
+    </Grid>
   );
 };
 
