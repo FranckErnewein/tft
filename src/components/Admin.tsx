beea7251ff98ac46eb1207601bf703dcf23d899f
Franck Ernewein <franck.ernewein@swile.co>
2023-02-05 18:53:43 +0900

    improve admin UI and add status message

--- src/components/Admin.tsx
+++ src/components/Admin.tsx
@@ -6,7 +6,9 @@ import startRound from "../commands/startRound";
 import endBet from "../commands/endBet";
 import endRound from "../commands/endRound";
 import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
 
 const StartGameButton = createCommandButton(startGame);
 const StartRoundButton = createCommandButton(startRound);
@@ -14,41 +16,61 @@ const EndBetButton = createCommandButton(endBet);
 const EndRoundButton = createCommandButton(endRound);
 
 const Admin: FC<Game> = (game) => {
+  let status = "";
+  let action = null;
+  if (!game.id) {
+    status = "No game. Create a new one.";
+    action = <StartGameButton>Start a new game</StartGameButton>;
+  } else if (!game.currentRound) {
+    status = "Waiting for new round.";
+    action = <StartRoundButton>Start a new round</StartRoundButton>;
+  } else if (game.currentRound.status === RoundStatus.BET_TIME) {
+    status = "Player are betting...";
+    action = <EndBetButton>End bets in 5s</EndBetButton>;
+  } else if (game.currentRound.status === RoundStatus.RUNNING) {
+    status = "Bet time over. choose a end for this round.";
+    action = (
+      <>
+        <Grid item xs={2}>
+          <EndRoundButton
+            color="secondary"
+            options={{ roundResult: RoundResult.LOSE }}
+          >
+            Round lost
+          </EndRoundButton>
+        </Grid>
+        <Grid item xs={2}>
+          <EndRoundButton options={{ roundResult: RoundResult.WIN }}>
+            Round won
+          </EndRoundButton>
+        </Grid>
+      </>
+    );
+  }
   return (
-    <Grid container>
-      <Grid item xs={12}>
-        <Typography>Admin</Typography>
-      </Grid>
-      <Grid item xs={4}>
-        <StartGameButton>
-          {game.id ? "reset game" : "start game"}
-        </StartGameButton>
-      </Grid>
-      {game.id && !game.currentRound && (
-        <Grid item xs={4}>
-          <StartRoundButton>Start a new round</StartRoundButton>
-        </Grid>
-      )}
-      {game.currentRound && game.currentRound?.status === RoundStatus.BET_TIME && (
-        <Grid item xs={4}>
-          <EndBetButton>End betting time</EndBetButton>
-        </Grid>
-      )}
-      {game.currentRound && game.currentRound?.status === RoundStatus.RUNNING && (
-        <>
-          <Grid item xs={2}>
-            <EndRoundButton options={{ roundResult: RoundResult.WIN }}>
-              Round won
-            </EndRoundButton>
+    <>
+      <Box>
+        <Typography variant="h3">Admin</Typography>
+      </Box>
+      <Paper>
+        <Box p={3}>
+          {game.currentRound && (
+            <Typography variant="h5">
+              Round n°{game.pastRounds.length + 1}
+            </Typography>
+          )}
+          <Typography variant="body1">{status}</Typography>
+          <Grid container mt={3}>
+            {action}
           </Grid>
-          <Grid item xs={2}>
-            <EndRoundButton options={{ roundResult: RoundResult.LOSE }}>
-              Round lost
-            </EndRoundButton>
-          </Grid>
-        </>
+        </Box>
+      </Paper>
+      {game.id && (
+        <Box p={3} textAlign="right">
+          <StartGameButton color="error">Reset this game</StartGameButton>
+        </Box>
       )}
-    </Grid>
+    </>
   );
 };
 
